437ca34b2b64ab796ed345331cb8dd9157a20ae8
unknown <zhanghongli@geoway.net>
2017-09-22 18:12:35 +0800

    Add gdal folder

---
+++ gdal/Clip.cs
@@ -0,0 +1,84 @@
+﻿using OSGeo.GDAL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace gdal
+{
+    class RasterClip
+    {
+        public void Clip()
+        {
+            //规则裁剪  
+            string srcFileName = @"D:\temp\test\17.tif";
+            string dstFileName = @"D:\测试\测试17-1.tif";
+
+            if (File.Exists(dstFileName))
+                File.Delete(dstFileName);
+
+
+            Dataset srcDs = Gdal.Open(srcFileName, Access.GA_ReadOnly);
+            DataType srcType = srcDs.GetRasterBand(1).DataType;
+
+            int bandCount = srcDs.RasterCount;
+
+            Console.WriteLine("原始影像数据类型是：{0}", srcType);
+            Console.WriteLine("原始影像的列数：{0}", srcDs.RasterXSize);
+            Console.WriteLine("原始影像的行数：{0}", srcDs.RasterYSize);
+
+
+            Console.WriteLine("请输入裁剪从第几列开始：");
+            int startXSize = Convert.ToInt32(0);
+            Console.WriteLine("请输入裁剪从第几行开始：");
+            int startYSize = Convert.ToInt32(0);
+
+            Console.WriteLine("请输入裁剪图像的宽度：");
+            int dstXSize = Convert.ToInt32(256);
+            Console.WriteLine("请输入裁剪图像的高度：");
+            int dstYSize = Convert.ToInt32(256);
+
+
+            double[] adfGeoTransform = new double[6];
+            srcDs.GetGeoTransform(adfGeoTransform);
+
+            //如果图像不含地理坐标信息，默认返回值是：(0,1,0,0,0,1)  
+            //左上角点坐标(padfGeoTransform[0],padfGeoTransform[3])；  
+            //padfGeoTransform[1]是像元宽度(影像在宽度上的分辨率)；  
+            //p/adfGeoTransform[5]是像元高度(影像在高度上的分辨率)；  
+            //如果影像是指北的,padfGeoTransform[2]和padfGeoTransform[4]这两个参数的值为0。  
+
+            Driver drv = Gdal.GetDriverByName("GTIFF");
+            Dataset dstDs = drv.Create(dstFileName, dstXSize, dstYSize, bandCount, srcType, null);
+
+            dstDs.SetGeoTransform(adfGeoTransform);
+            dstDs.SetProjection(srcDs.GetProjectionRef());
+
+            int[] bandArray = new int[bandCount];
+            for (int i = 0; i < bandCount; i++)
+            {
+                bandArray[i] = i + 1;
+            }
+
+            //if (srcType == DataType.GDT_UInt16)
+            {
+                int[] dataArray = new int[dstXSize * dstYSize * bandCount];
+
+                srcDs.ReadRaster(startXSize, startYSize, dstXSize, dstYSize, dataArray, dstXSize, dstYSize, bandCount, bandArray, 0, 0, 0);
+                dstDs.WriteRaster(0, 0, dstXSize, dstYSize, dataArray, dstXSize, dstYSize, bandCount, bandArray, 0, 0, 0);
+
+                dstDs.FlushCache();
+            }
+
+            Console.WriteLine("success");
+            Console.WriteLine();
+            Console.WriteLine("裁剪影像的宽为：{0}，高为:{1}。", dstDs.RasterXSize, dstDs.RasterYSize);
+
+            //最后释放资源  
+            srcDs.Dispose();
+            dstDs.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ gdal/Program.cs
@@ -0,0 +1,133 @@
+﻿using OSGeo.GDAL;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace gdal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//调用
+            TilesBounds tilesBounds = new TilesBounds();
+            tilesBounds.minCol = 109196;
+            tilesBounds.maxCol = 109217;
+            tilesBounds.minRow = 53340;
+            tilesBounds.maxRow = 53355;
+            tilesBounds.zoomLevel = 17;
+
+            GdalConfiguration.ConfigureGdal();
+
+            GdalConfiguration.ConfigureOgr();
+            var clip = new RasterClip();
+            clip.Clip();
+            Console.ReadKey();
+            //string outPutFileName = @"D:\temp\test\" + tilesBounds.zoomLevel.ToString() + ".tif";
+            //string tilePath = @"D:\temp\test\";
+            //TilesCombine combine = new TilesCombine();
+            //combine.CombineTiles(tilesBounds, tilePath, outPutFileName);
+        }
+    }
+
+    class TilesCombine
+    {
+        void SaveBitmapBuffered(Dataset src, Dataset dst, int x, int y)
+        {
+            if (src.RasterCount < 3)
+            {
+                System.Environment.Exit(-1);
+            }
+
+            // Get the GDAL Band objects from the Dataset
+            Band redBand = src.GetRasterBand(1);
+            Band greenBand = src.GetRasterBand(2);
+            Band blueBand = src.GetRasterBand(3);
+            
+            // Get the width and height of the raster
+            int width = redBand.XSize;
+            int height = redBand.YSize;
+
+            byte[] r = new byte[width * height];
+            byte[] g = new byte[width * height];
+            byte[] b = new byte[width * height];
+
+            redBand.ReadRaster(0, 0, width, height, r, width, height, 0, 0);
+            greenBand.ReadRaster(0, 0, width, height, g, width, height, 0, 0);
+            blueBand.ReadRaster(0, 0, width, height, b, width, height, 0, 0);
+
+            Band wrb = dst.GetRasterBand(1);
+            wrb.WriteRaster(x * width, y * height, width, height, r, width, height, 0, 0);
+            Band wgb = dst.GetRasterBand(2);
+            wgb.WriteRaster(x * width, y * height, width, height, g, width, height, 0, 0);
+            Band wbb = dst.GetRasterBand(3);
+            wbb.WriteRaster(x * width, y * height, width, height, b, width, height, 0, 0);
+        }
+
+        /// <summary>
+        /// 拼接瓦片
+        /// </summary>
+        /// <param name="tilesBounds"></param>
+        /// <param name="tilePath"></param>
+        /// <param name="outPutFileName"></param>
+        public void CombineTiles(TilesBounds tilesBounds, string tilePath, string outPutFileName)
+        {
+            if (File.Exists(outPutFileName))
+            {
+                File.Delete(outPutFileName);
+            }
+            int imageWidth = 256 * (tilesBounds.maxCol - tilesBounds.minCol + 1);
+            int imageHeight = 256 * (tilesBounds.maxRow - tilesBounds.minRow + 1);
+           
+            //Register driver(s). 
+            //Gdal.AllRegister();
+            Driver driver = Gdal.GetDriverByName("GTiff");
+            Dataset destDataset = driver.Create(outPutFileName, imageWidth, imageHeight, 3, DataType.GDT_Byte, null);
+
+
+            //for (int col = tilesBounds.minCol; col <= tilesBounds.maxCol; col++)
+            //{
+            //    for (int row = tilesBounds.minRow; row <= tilesBounds.maxRow; row++)
+            //    {
+                    try
+                    {
+                        //string sourceFileName = tilePath + tilesBounds.zoomLevel.ToString() + "\\" + col.ToString() + "\\" + row.ToString() + ".png";
+                        var sourceFileName = @"D:\temp\test\4\4_2007_887.jpg";
+                        if (File.Exists(sourceFileName))
+                        {
+                            Dataset sourceDataset = Gdal.Open(sourceFileName, Access.GA_ReadOnly);
+                            
+                            if (sourceDataset != null)
+                            {
+                                SaveBitmapBuffered(sourceDataset, destDataset, 0, 0);
+                            }
+                        }
+                        sourceFileName = @"D:\temp\test\4\4_2008_887.jpg";
+                        var sourceDataset2 = Gdal.Open(sourceFileName, Access.GA_ReadOnly);
+                        if (sourceDataset2 != null)
+                        {
+                            SaveBitmapBuffered(sourceDataset2, destDataset, 1, 0);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+            //    }
+            //}
+            destDataset.Dispose();
+
+        }
+    }
+
+    class TilesBounds
+    {
+        public int minCol { get; set; }
+        public int maxCol { get; set; }
+        public int minRow { get; set; }
+        public int maxRow { get; set; }
+        public int zoomLevel { get; set; }
+    }
+}
